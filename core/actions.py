bc6659167fdf3f2c9e6cb3c0256eb92bf95f310b
BoppreH <boppreh@gmail.com>
2013-04-21 23:17:43 -0300

    Add docs to core package.

--- core/actions.py
+++ core/actions.py
@@ -1,18 +1,69 @@
+"""
+Module for editor actions.
+"""
 class Action(object):
+    """
+    Base action type, capable of executing an arbitrary action on an editor and
+    undoing it if necessary.
+    """
+    def is_available(self, editor):
+        """
+        Returns True if the action represented by this instance can be executed
+        on the given editor.
+        """
+        return self._is_available(editor,
+                                  editor.selected,
+                                  editor.selected.parent)
+
+    def _is_avaible(self, editor, selected, parent):
+        """
+        Auxiliary function to be overridden by its subclasses. 'selected' and
+        'parent' are just easy to access references, also accessible from the
+        'editor' instance.
+        """
+        pass
+
     def execute(self, editor):
+        """
+        Executes an action on an editor, remembering the previously selected
+        item in case it needs to be rolled back. Returns the node that should
+        be new selected item.
+        """
         self.previous_selected = editor.selected
         return self._execute(editor, editor.selected, editor.selected.parent)
 
     def rollback(self, editor):
+        """
+        Undoes the action done by execute, returning the item selected before
+        the 'execute' call.
+        """
         self._rollback(editor, editor.selected, editor.selected.parent)
         return self.previous_selected
 
+    def _execute(self, editor, selected, parent):
+        """
+        Auxiliary function to be overridden by its subclasses. 'selected' and
+        'parent' are just easy to access references, also accessible from the
+        'editor' instance.
+
+        Return value is passed to editor to become the new selected item.
+        """
+        pass
+
     def _rollback(self, editor, selected, parent):
+        """
+        Auxiliary function to be overridden by its subclasses. 'selected' and
+        'parent' are just easy to access references, also accessible from the
+        'editor' instance.
+
+        Return value is ignored.
+        """
         pass
 
 
+
 class SelectNextSibling(Action):
-    def is_available(self, editor, selected, parent):
+    def _is_available(self, editor, selected, parent):
         return parent is not None and parent.selected_index < len(parent) - 1
 
     def _execute(self, editor, selected, parent):
@@ -20,7 +71,7 @@ class SelectNextSibling(Action):
 
 
 class SelectPrevSibling(Action):
-    def is_available(self, editor, selected, parent):
+    def _is_available(self, editor, selected, parent):
         return parent is not None and parent.selected_index > 0
 
     def _execute(self, editor, selected, parent):
@@ -28,7 +79,7 @@ class SelectPrevSibling(Action):
 
 
 class SelectParent(Action):
-    def is_available(self, editor, selected, parent):
+    def _is_available(self, editor, selected, parent):
         return parent is not None
 
     def _execute(self, editor, selected, parent):
@@ -36,7 +87,7 @@ class SelectParent(Action):
 
 
 class SelectChild(Action):
-    def is_available(self, editor, selected, parent):
+    def _is_available(self, editor, selected, parent):
         return (hasattr(selected, 'selected_index')
                 and len(selected) > 0
                 and selected[selected.selected_index].__class__ != str)
@@ -46,8 +97,8 @@ class SelectChild(Action):
 
 
 class MoveUp(SelectPrevSibling):
-    def is_available(self, editor, selected, parent):
-        return (SelectPrevSibling.is_available(self, editor, selected, parent)
+    def _is_available(self, editor, selected, parent):
+        return (SelectPrevSibling._is_available(self, editor, selected, parent)
                 and hasattr(parent, 'insert'))
 
     def _execute(self, editor, selected, parent):
@@ -57,8 +108,8 @@ class MoveUp(SelectPrevSibling):
 
 
 class MoveDown(SelectNextSibling):
-    def is_available(self, editor, selected, parent):
-        return (SelectNextSibling.is_available(self, editor, selected, parent)
+    def _is_available(self, editor, selected, parent):
+        return (SelectNextSibling._is_available(self, editor, selected, parent)
                 and hasattr(parent, 'insert'))
 
     def _execute(self, editor, selected, parent):
@@ -69,7 +120,7 @@ class MoveDown(SelectNextSibling):
     
 from copy import deepcopy
 class Copy(Action):
-    def is_available(self, editor, selected, parent):
+    def _is_available(self, editor, selected, parent):
         return True 
 
     def _execute(self, editor, selected, parent):
@@ -82,8 +133,11 @@ class Copy(Action):
 
 
 class Paste(Action):
-    def is_available(self, editor, selected, parent):
-        return editor.clipboard is not None and hasattr(selected, 'insert') and selected.can_insert(selected.selected_index + 1, editor.clipboard)
+    def _is_available(self, editor, selected, parent):
+        return (editor.clipboard is not None
+                and hasattr(selected, 'insert')
+                and selected.can_insert(selected.selected_index + 1,
+                                        editor.clipboard))
 
     def _execute(self, editor, selected, parent):
         copy = deepcopy(editor.clipboard)
@@ -92,7 +146,7 @@ class Paste(Action):
 
 
 class Delete(Action):
-    def is_available(self, editor, selected, parent):
+    def _is_available(self, editor, selected, parent):
         return hasattr(parent, 'remove')
 
     def _execute(self, editor, selected, parent):
@@ -120,8 +174,10 @@ class Insert(Action):
     def __init__(self, structure_class):
         self.structure_class = structure_class
 
-    def is_available(self, editor, selected, parent):
-        return hasattr(selected, 'append') and selected.can_insert(selected.selected_index + 1, self.structure_class)
+    def _is_available(self, editor, selected, parent):
+        return (hasattr(selected, 'append')
+                and selected.can_insert(selected.selected_index + 1,
+                                        self.structure_class))
 
     def _execute(self, editor, selected, parent):
         new_item = self.structure_class()
@@ -133,7 +189,7 @@ class Select(Action):
     def __init__(self, node):
         self.node = node
 
-    def is_available(self):
+    def _is_available(self, editor, selected, parent):
         return True
 
     def _execute(self, editor, selected, parent):
--- core/editor.py
+++ core/editor.py
@@ -3,14 +3,31 @@ Module for editing a program's source code interactively with a structured
 editor.
 """
 class Editor(object):
+    """
+    Class for an abstract code editor. Supports execution of arbitrary actions,
+    undo/redo, clipboard attribute (to be used by actions), node selection
+    (single selection only for the moment) and rendering the tree with a
+    user-specified function running on every node's text.
+    """
     def __init__(self, root):
+        """
+        Initializes an editor from an existing root node, selecting the root
+        node, with empty clipboard and undo/redo history.
+        """
         self.root = root
         self.selected = root
         self.clipboard = None
         self.past_history = []
         self.future_history = []
 
-    def update_selected(self, new_selected):
+    def _update_selected(self, new_selected):
+        """
+        Changes the currently selected node, updating the parent's
+        selected_index value if it has a parent.
+
+        The selected_index is stored in the node itself so it can be restored
+        when the user navigates back to it.
+        """
         self.selected = new_selected
 
         parent = self.selected.parent
@@ -18,20 +35,44 @@ class Editor(object):
             parent.selected_index = parent.index(self.selected)
 
     def execute(self, action):
-        self.update_selected(action.execute(self))
+        """
+        Executes a new action, allowing it to be reversed by undo. To avoid
+        forking the action history, all actions undone and not redone are lost.
+
+        Actions must have 'is_available', 'execute' and 'rollback' methods.
+        """
+        self._update_selected(action.execute(self))
         self.past_history.append(action)
         self.future_history = []
 
     def is_available(self, action):
+        """
+        Checks if a given action can be executed with the current editor state.
+        """
         return action.is_available(self, self.selected, self.selected.parent)
 
     def redo(self):
+        """
+        Re-executes the last undone action. This is not considered an action by
+        itself.
+        """
         self.past_history.append(self.future_history.pop(0))
-        self.update_selected(self.past_history[-1].execute(self))
+        self._update_selected(self.past_history[-1].execute(self))
 
     def undo(self):
+        """
+        Rollbacks the last action done. This is not considered an action by
+        itself.
+        """
         self.future_history.insert(0, self.past_history.pop())
-        self.update_selected(self.future_history[0].rollback(self))
+        self._update_selected(self.future_history[0].rollback(self))
 
     def render(self, wrapper=None):
+        """
+        Returns the textual representation of the entire tree (not just the
+        selected node).
+
+        'wrapper' should be a function that takes a node's reference and its
+        basic rendering and returns the complete rendering of that node.
+        """
         return self.root.render(wrapper)
