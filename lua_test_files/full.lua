1b3090099da1f85d6b9b1812b095af92b150806a
BoppreH <boppreh@gmail.com>
2013-05-01 00:01:00 -0300

    Update todo and inclue full parser test file

---
+++ lua_test_files/full.lua
@@ -0,0 +1,37 @@
+a = {b={}, print; [2*1]=2}
+function a.b.c(i, j, ...)
+    repeat until 1 >= 1
+    repeat
+        d = 5
+        for a in {} do end
+        for a, c in {}, 3 do
+              print("")
+        end
+    until 1 or not 1
+end
+
+if i ~= (2 * 3) % -a[2] then
+    local function x() end
+    local function y(i) 
+        local l, m = print"", a[1 and 3]("a" .. "", nil);
+    end
+elseif a[1]:b() ~= #3 then
+elseif 2 <= function() local d end then
+    return
+else
+end
+
+while false do end
+while false and a do
+    for x = a.b:c{}, 10 do end
+    for x = 2*3, 10, 8 do
+          if false then
+          else
+                do
+                  if true then end
+                  return {}, function(...) end
+              end
+          end
+    end
+end
+
