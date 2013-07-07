6b2699b9e0b5147992193fafd68258608339f75d
BoppreH <boppreh@gmail.com>
2013-07-06 21:37:18 -0300

    Add tests for language and grammar parsing

--- test_language.py
+++ test_language.py
@@ -71,5 +71,39 @@ def test_serialization():
     assert str(ListNode([StrNode('a', type_a)], type_a)) == '%%a%%'
     assert str(DictNode({0: StrNode('a', type_a)}, type_a)) == '%%a%%'
 
+def test_language():
+    type_a = NodeType('a', '.+', None)
+    type_a.output_template = '{}'
+    type_b = NodeType('b', type_a, None)
+    type_b.output_template = '{}'
+    type_c = NodeType('c', [type_a, type_b], None)
+    type_c.output_template = '{} {}'
+    l = Language({'a': type_a, 'b': type_b, 'c': type_c}, None)
+
+    assert str(l.parse_xml('<a>1</a>')) == '1'
+    assert str(l.parse_xml('<b></b>')) == ''
+    assert str(l.parse_xml('<b> <a>1</a> </b>')) == '1'
+    assert str(l.parse_xml('<b> <a>1</a> <a>1</a> </b>')) == '1, 1'
+    assert str(l.parse_xml('<c> <b> <a>1</a> </b> <a>1</a> </c>')) == '1 1'
+
+def test_parse_grammar():
+    pairs = [('a', r'/.+/'), ('b(a)', r'/\w+/'), ('c', 'b*'), ('d', 'a c')]
+    n = parse_grammar(pairs)
+
+    assert n['a'].rule == r'.+'
+    assert n['b'].rule == r'\w+'
+    assert n['c'].rule == n['b']
+    assert n['d'].rule == [n['a'], n['c']]
+
+    assert n['b'].parent == n['a']
+
+def test_read_language():
+    from ConfigParser import RawConfigParser
+    c = RawConfigParser()
+    c.add_section('Composition Rules')
+    c.set('Composition Rules', 'a', '/.+/')
+    c.set('Composition Rules', 'b(a)', '/\w+/')
+    c.set('Composition Rules', 'b(a)', '/\w+/')
+
 
 pytest.main()
