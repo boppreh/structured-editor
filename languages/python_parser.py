4842dbd1e34017ca60a4849bf599620bf30bdefd
boppreh <boppreh@gmail.com>
2014-01-01 18:57:42 -0200

    Add test support for `Module` Python nodes

--- languages/python_parser.py
+++ languages/python_parser.py
@@ -1,8 +1,33 @@
 from .structures import *
-from ast import parse
+import ast
+
+def make_ids():
+    i = 0
+    while True:
+        yield i
+        i += 1
+ids = make_ids()
+
+ast.Module.__len__ = lambda self: len(self.body)
+ast.Module.index = lambda self, value: self.body.index(value)
+ast.Module.__getitem__ = lambda self, index: self.body[index]
+def s(self, index, value):
+    self.body[index] = value
+ast.Module.__setitem__ = s
+ast.Module.render = lambda self, wrapper: wrapper(self).format('\n'.join(map(str, self)))
+
+def convert(node, parent=None):
+    node.parent = parent
+    node.template = '{}'
+    node.node_id = ids.__next__()
+    node.defaulted = []
+    if isinstance(node, ast.Module):
+        for body_part in node.body:
+            convert(body_part, node)
+    return node
 
 def parse_string(string):
-    return parse(string)
+    return convert(ast.parse(string))
 
 def new_empty():
     return parse_string('')
